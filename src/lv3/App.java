a42723cda5b0076f7ae3132583631c81bd22dc08
Yeon <yeoni9094@naver.com>
2024-10-17 13:17:15 +0900

    feat: 리스트에 연산결과 담기
    Calculator 클래스에 리스트 컬렉션을 추가하여 연산 결과가 리스트에 저장되도록 수정

--- src/lv3/App.java
+++ src/lv3/App.java
@@ -1,5 +1,7 @@
 package lv3;
 
+import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class App {
@@ -7,17 +9,29 @@ public class App {
         Scanner sc = new Scanner(System.in);
         ArithmeticCalculator<Double> calculator = new ArithmeticCalculator<>();
 
-        System.out.print("첫 번째 정수를 입력하세요: ");
-        double firstNumber = sc.nextDouble();
+        while (true) {
+            System.out.print("첫 번째 정수를 입력하세요: ");
+            double firstNumber = sc.nextDouble();
 
-        System.out.print("두 번째 정수를 입력하세요: ");
-        double secondNumber = sc.nextDouble();
+            System.out.print("두 번째 정수를 입력하세요: ");
+            double secondNumber = sc.nextDouble();
 
-        System.out.print("사칙연산 기호를 입력하세요: ");
-        char operator = sc.next().charAt(0);
-        OperatorType operatorType = OperatorType.getType(operator);
-        
-        double result = calculator.calculate(firstNumber, secondNumber, operatorType);
-        System.out.println("result = " + result);
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            char operator = sc.next().charAt(0);
+            OperatorType operatorType = OperatorType.getType(operator);
+
+            calculator.calculate(firstNumber, secondNumber, operatorType); // 사칙연산 계산
+            calculator.setCalculate(); // 결과값 수정
+            calculator.getCalculate(); // 결과값 반환
+
+            // 계산이 끝나면 더 계산할지 말지 입력 받음
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            String text = sc.next();
+
+            if (Objects.equals(text, "exit")) {
+                System.out.println("계산을 종료합니다.");
+                break;
+            }
+        }
     }
 }
--- src/lv3/ArithmeticCalculator.java
+++ src/lv3/ArithmeticCalculator.java
@@ -1,6 +1,10 @@
 package lv3;
 
+import java.util.ArrayList;
+
 public class ArithmeticCalculator<T extends Number> {
+    // 연산 결과를 담을 리스트 생성
+    ArrayList<Double> list = new ArrayList<>();
 
     // 연산자 파라미터를 enum 클래스의 열거 상수로 받음
     public double calculate(T firstNumber, T secondNumber, OperatorType operator) throws IllegalArgumentException {
@@ -11,18 +15,22 @@ public class ArithmeticCalculator<T extends Number> {
         switch (operator) {
             case PLUS:
                 result = x + y;
+                list.add(result);
                 break;
             case MINUS:
                 result = x - y;
+                list.add(result);
                 break;
             case MULTI:
                 result = x * y;
+                list.add(result);
                 break;
             case DIVIDE:
                 if (y == 0) {
                     System.out.println("0으로 나눌 수 없습니다.");
                 } else {
                     result = x / y;
+                    list.add(result);
                 }
                 break;
             default:
@@ -30,5 +38,15 @@ public class ArithmeticCalculator<T extends Number> {
         }
 
         return result;
+    }
+
+    public void getCalculate() {
+        System.out.println("결과: " + list.get(0));
+    }
+
+    public void setCalculate() {
+        for (double result : list) {
+            list.set(0, result);
+        }
     }
 }
